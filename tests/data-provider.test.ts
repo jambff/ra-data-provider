d489b05444ec08aaeb53ff142f75cf459c1d58f8
Alex Mendes <alexanderhmendes@gmail.com>
2023-02-11 18:51:02 +0000

    test: add a few more tests

--- tests/data-provider.test.ts
+++ tests/data-provider.test.ts
@@ -1,5 +1,10 @@
 import nock from 'nock';
+import unfetch from 'unfetch';
 import { createDataProvider } from '../src';
+
+jest.mock('unfetch', () =>
+  jest.fn(async (...args) => jest.requireActual('unfetch')(...args)),
+);
 
 const apiBaseUrl = 'http://api.com';
 
@@ -16,21 +21,72 @@ nock(apiBaseUrl)
   .reply(200, {
     items: [{ title: 'Item one' }],
     total: 1,
-  });
+  })
+  .get('/resource/1')
+  .query(true)
+  .reply(200, { title: 'Item one' });
 
 describe('Data Provider', () => {
-  it('gets a list', async () => {
-    const dataProvider = createDataProvider(apiBaseUrl);
+  describe('getList', () => {
+    it('gets a list', async () => {
+      const dataProvider = createDataProvider(apiBaseUrl);
 
-    const result = await dataProvider.getList('resource', {
-      pagination: { page: 1, perPage: 25 },
-      sort: { field: 'somefield', order: 'asc' },
-      filter: {},
+      const result = await dataProvider.getList('resource', {
+        pagination: { page: 1, perPage: 25 },
+        sort: { field: 'somefield', order: 'asc' },
+        filter: {},
+      });
+
+      expect(result).toEqual({
+        data: [{ title: 'Item one' }],
+        total: 1,
+      });
+
+      expect(unfetch).toHaveBeenCalledTimes(1);
+      expect(unfetch).toHaveBeenCalledWith(
+        'http://api.com/resource?limit=25&offset=0&sort[somefield]=asc',
+      );
+    });
+  });
+
+  describe('getOne', () => {
+    it('gets one item', async () => {
+      const dataProvider = createDataProvider(apiBaseUrl);
+
+      const result = await dataProvider.getOne('resource', { id: 1 });
+
+      expect(result).toEqual({ data: { title: 'Item one' } });
+      expect(unfetch).toHaveBeenCalledTimes(1);
+      expect(unfetch).toHaveBeenCalledWith('http://api.com/resource/1');
     });
 
-    expect(result).toEqual({
-      data: [{ title: 'Item one' }],
-      total: 1,
+    it('gets one item with includes', async () => {
+      const dataProvider = createDataProvider(apiBaseUrl);
+
+      const result = await dataProvider.getOne('resource', {
+        id: 1,
+        meta: { include: { foo: true, bar: ['baz'] } },
+      });
+
+      expect(result).toEqual({ data: { title: 'Item one' } });
+      expect(unfetch).toHaveBeenCalledTimes(1);
+      expect(unfetch).toHaveBeenCalledWith(
+        'http://api.com/resource/1?include[foo]=true&include[bar][]=baz',
+      );
+    });
+  });
+
+  describe('getMany', () => {
+    it('gets many items', async () => {
+      const dataProvider = createDataProvider(apiBaseUrl);
+
+      const result = await dataProvider.getMany('resource', { ids: [1, 2, 3] });
+
+      expect(result).toEqual({ data: [{ title: 'Item one' }] });
+      expect(unfetch).toHaveBeenCalledTimes(1);
+      expect(unfetch).toHaveBeenCalledWith(
+        'http://api.com/resource?id[]=1&id[]=2&id[]=3',
+      );
     });
   });
 });
