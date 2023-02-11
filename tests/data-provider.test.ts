8fcfdd384c5119393a611526eecb8b34e204e642
Alex Mendes <alexanderhmendes@gmail.com>
2023-02-11 23:11:03 +0000

    test: add a few more tests

--- tests/data-provider.test.ts
+++ tests/data-provider.test.ts
@@ -22,8 +22,14 @@ nock(apiBaseUrl)
     items: [{ title: 'Item one' }],
     total: 1,
   })
+  .post('/resource')
+  .reply(201, { title: 'Item one' })
   .get('/resource/1')
   .query(true)
+  .reply(200, { title: 'Item one' })
+  .options('/resource/1')
+  .reply(200)
+  .put('/resource/1')
   .reply(200, { title: 'Item one' });
 
 describe('Data Provider', () => {
@@ -87,6 +93,64 @@ describe('Data Provider', () => {
       expect(unfetch).toHaveBeenCalledWith(
         'http://api.com/resource?id[]=1&id[]=2&id[]=3',
       );
+    });
+  });
+
+  describe('getManyReference', () => {
+    it('gets many references', async () => {
+      const dataProvider = createDataProvider(apiBaseUrl);
+
+      const result = await dataProvider.getManyReference('resource', {
+        target: 'sometarget',
+        id: 1,
+        pagination: { page: 1, perPage: 25 },
+        sort: { field: 'somefield', order: 'asc' },
+        filter: {},
+      });
+
+      expect(result).toEqual({ data: [{ title: 'Item one' }], total: 1 });
+      expect(unfetch).toHaveBeenCalledTimes(1);
+      expect(unfetch).toHaveBeenCalledWith(
+        'http://api.com/resource?limit=25&offset=0&sort[somefield]=asc',
+      );
+    });
+  });
+
+  describe('create', () => {
+    it('creates an item', async () => {
+      const dataProvider = createDataProvider(apiBaseUrl);
+
+      const result = await dataProvider.create('resource', {
+        data: { foo: 'bar' },
+      });
+
+      expect(result).toEqual({ data: { title: 'Item one' } });
+      expect(unfetch).toHaveBeenCalledTimes(1);
+      expect(unfetch).toHaveBeenCalledWith('http://api.com/resource', {
+        method: 'POST',
+        body: JSON.stringify({ foo: 'bar' }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('updates an item', async () => {
+      const dataProvider = createDataProvider(apiBaseUrl);
+
+      const result = await dataProvider.update('resource', {
+        id: 1,
+        data: { foo: 'bar' },
+        previousData: { foo: 'baz' },
+      });
+
+      expect(result).toEqual({ data: { title: 'Item one' } });
+      expect(unfetch).toHaveBeenCalledTimes(1);
+      expect(unfetch).toHaveBeenCalledWith('http://api.com/resource/1', {
+        method: 'PUT',
+        body: JSON.stringify({ foo: 'bar' }),
+        headers: { 'Content-Type': 'application/json' },
+      });
     });
   });
 });
